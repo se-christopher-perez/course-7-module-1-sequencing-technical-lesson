726defe971313572ae829edf183783e0bc3e0d8b
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-03 21:33:11 -0400

    update

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from tasks.task_manager import display_tasks, filter_tasks, task_generator
 
 # Initial list of tasks
-tasks = ["Buy groceries", "Finish project", "Call mom", "Send email", "Clean room"]
+tasks = ["Buy groceries", "Finish project", "Call mom", "Send email", "Clean room", "Data Project", "Home Project"]
 
-print("\nAll Tasks:")
-display_tasks(tasks)
+# print("\nAll Tasks:")
+# display_tasks(tasks)
 
+# filter_tasks(tasks, "project")
+
+project_tasks = task_generator(tasks, "project")
+next(project_tasks)
+next(project_tasks)
+print(next(project_tasks))
